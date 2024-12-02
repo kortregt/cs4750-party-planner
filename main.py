ce32fe6747fe9d6973299c7f6f5e59446483bf45
kortregt <42360223+kortregt@users.noreply.github.com>
2024-12-02 03:56:23 -0500

    again

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from routes import staff, venues, events, functions
 from fastapi.templating import Jinja2Templates
