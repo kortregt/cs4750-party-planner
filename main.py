b13d232e72edce20ef398d8e7189dc5e55e91532
kortregt <42360223+kortregt@users.noreply.github.com>
2024-12-02 05:12:14 -0500

    added CRUD for customers and fixed bootstrap styling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from routes import staff, venues, events, functions
+from routes import staff, venues, events, functions, customers
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -10,6 +10,7 @@ app.include_router(staff.router)
 app.include_router(venues.router)
 app.include_router(events.router)
 app.include_router(functions.router)
+app.include_router(customers.router)
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
